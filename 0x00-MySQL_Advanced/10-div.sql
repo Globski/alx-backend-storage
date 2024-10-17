c42c83678f751e6beaef1ae88948c5addb4e93c9
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 11:07:57 -0700

    Add 10-div.sql: Create SafeDiv function for safe division.

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,11 +1,9 @@
--- Create a function to safely divide two integers
--- Defines a function SafeDiv that takes two integers.
--- Returns 0 if the second argument is 0; otherwise, it returns the division of a by b.
+-- Create a function to safely divide two integers.
+-- This function SafeDiv takes two integers as arguments.
+-- It returns 0 if the second argument is 0; otherwise, it returns the result of a divided by b.
 CREATE FUNCTION SafeDiv(a INT, b INT) 
 RETURNS FLOAT
 DETERMINISTIC
 BEGIN
     RETURN IF(b = 0, 0, a / b);
 END //
-
-DELIMITER ;
