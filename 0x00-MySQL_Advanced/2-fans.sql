f4dc3a27003a1c3502e20dd221492d5d5ea533bc
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 10:02:28 -0700

    Add 2-fans.sql: Rank countries by number of band fans.

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,11 @@
+-- This script retrieves and ranks the origins of metal bands
+-- based on the number of fans from the imported 'metal_bands' table.
+
+-- The result will include:
+-- 1. origin: The country where the band is from.
+-- 2. nb_fans: The total number of fans from that country.
+
+SELECT origin, COUNT(*) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
