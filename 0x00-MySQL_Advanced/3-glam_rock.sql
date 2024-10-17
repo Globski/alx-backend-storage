e426b7654bc0c98407fbba522e320e3112401b15
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 10:12:22 -0700

    Add 3-glam_rock.sql: List Glam rock bands ranked by longevity.

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,14 @@
 -- This script retrieves and ranks bands whose main style is Glam rock
--- based on their longevity calculated until the year 2022.
+-- based on their longevity, calculated until the year 2022.
 
 -- The result will include:
 -- 1. band_name: The name of the band.
 -- 2. lifespan: The number of years the band has been active until 2022.
 
-SELECT 
-    band_name,
-    YEAR(2022) - formed AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+SET @current_year = 2022;
+
+SELECT band_name,
+       (@current_year - IFNULL(formed, @current_year)) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
