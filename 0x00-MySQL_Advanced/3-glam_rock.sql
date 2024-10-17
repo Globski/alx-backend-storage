25857479e60240296e8b20d6b29096178978d84f
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 10:15:51 -0700

    Add 3-glam_rock.sql: List Glam rock bands ranked by longevity.

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,12 +3,10 @@
 
 -- The result will include:
 -- 1. band_name: The name of the band.
--- 2. lifespan: The number of years the band has been active until 2022.
+-- 2. lifespan: The number of years the band has been active until 2022,
+--    calculated as (2022 - formed). If the formed year is null, it defaults to 2022.
 
-SET @current_year = 2022;
-
-SELECT band_name,
-       (@current_year - IFNULL(formed, @current_year)) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
